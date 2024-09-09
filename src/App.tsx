46cdc66aded0d8edccbc3d3187c27e4070671df2
Sofia Bargues <barguessofia@gmail.com>
2024-09-09 07:17:12 +0100

    presionar botton dos veces

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import "./App.css";
 
 function Box({ text: text, onClick }: { text: number; onClick: () => void }) {
   return (
-    <button onClick={onClick} className="border-2 h-7 w-7">
+    <button
+      disabled={text ? true : false}
+      onClick={onClick}
+      className="border-2 h-7 w-7"
+    >
       {text}
     </button>
   );
 }
 function App() {
   const arrBoxes = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-  const [carta1, setCarta1] = useState(null);
-  const [carta2, setCarta2] = useState(null);
+  const [indexCarta1, setIndexCarta1] = useState(null);
+  const [indexCarta2, setIndexCarta2] = useState(null);
   const [adivinadas, setAdivinadas] = useState([]);
-  console.log(adivinadas);
   const handleBoxClick = (index: number) => {
-    if (carta1 === null) {
-      setCarta1(index);
-    } else if (carta1 !== null && carta2 === null) {
-      setCarta2(index);
+    if (indexCarta1 === null) {
+      setIndexCarta1(index);
+    } else if (indexCarta1 !== null && indexCarta2 === null) {
+      setIndexCarta2(index);
     }
   };
 
@@ -28,20 +31,27 @@ function App() {
         {arrBoxes.map((num, i) => (
           <Box
             key={i}
-            text={adivinadas.includes(num) ? num : ""}
-            onClick={() => handleBoxClick(num)}
+            text={
+              adivinadas.includes(num) || indexCarta1 === i || indexCarta2 === i
+                ? num
+                : ""
+            }
+            onClick={() => handleBoxClick(i)}
           />
         ))}
       </div>
       <button
         onClick={() => {
-          if (carta1 === carta2 && carta1 != null) {
-            setAdivinadas([...adivinadas, carta1]);
-            setCarta1(null);
-            setCarta2(null);
+          if (
+            arrBoxes[indexCarta1] === arrBoxes[indexCarta2] &&
+            arrBoxes[indexCarta1] != null
+          ) {
+            setAdivinadas([...adivinadas, arrBoxes[indexCarta1]]);
+            setIndexCarta1(null);
+            setIndexCarta2(null);
           } else {
-            setCarta1(null);
-            setCarta2(null);
+            setIndexCarta1(null);
+            setIndexCarta2(null);
           }
         }}
       >
